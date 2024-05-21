f8a00c6e8ebbe262bc1d08f59c696a416e73c62c
Tyler Patenaude <witwityler@gmail.com>
2024-05-20 18:29:00 -0700

    Minor changes

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import os
+import time
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
+browser = webdriver.Firefox()
 
 # Main function
 def main():
-    print("Hello World!")
+    browser.get("https://www.youtube.com")
+    hamburgerMenu = browser.find_element(By.ID, 'guide-button')
+    time.sleep(0.5)
+    hamburgerMenu.click()
     
+    
+    
+    
+    
+    
+    # Shuts down browser tab automatically, could be usefullater
+    # driver.quit()
     
 if __name__ == "__main__":
     main()  
